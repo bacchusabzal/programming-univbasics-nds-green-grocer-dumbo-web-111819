33f487be2630534fe3c2d521c9b9ea288abff13e
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-15 02:26:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,22 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  
+   consolidated_cart = {}
+  
+  cart.each do |item|
+    item_name = item.keys[0]
+    
+    consolidated_cart[item_name] = item.values[0]
+
+    if consolidated_cart[item_name][:count] 
+      consolidated_cart[item_name][:count] += 1
+    else
+      consolidated_cart[item_name][:count] = 1
+    end
+  end
+
+  consolidated_cart
 end
 
 def consolidate_cart(cart)
@@ -9,6 +25,7 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  
 end
 
 def apply_coupons(cart, coupons)
